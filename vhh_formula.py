9345bd60d6dff219da2970b137c579ccf0b2b19b
ChuyuanLiu <chuyuanliu96@gmail.com>
2023-05-25 10:51:31 -0400

    update

--- vhh_formula.py
+++ vhh_formula.py
@@ -1,21 +1,23 @@
+import re
+
 from heptools.physics import FormulaXS
 
+
 class VVHH_Vertex_LO(FormulaXS):
-    _pattern = r'CV_{CV}_C2V_{C2V}_C3_{C3}'
-    _diagram = ['CV', 'C2V', 'C3'], [[1, 0, 1], [2, 0, 0], [0, 1, 0]]
+    diagrams = ['CV', 'C2V', 'C3'], [[1, 0, 1], [2, 0, 0], [0, 1, 0]]
+    search_pattern = r'((C3_(?P<C3>\d+_\d+))|(C2V_(?P<C2V>\d+_\d+))|(CV_(?P<CV>\d+_\d+))|_)+'
 
-    _decimal_separator = '_'
-    _decimal_pattern   = '{:.1f}'
-
-    # # for datacard
-    # _decimal_separator = 'p'
-    # _decimal_pattern   = '{:.1g}'
+    # # datacard
+    # number_separator = 'p'
+    # number_pattern   = '{:.1g}'
 
 class ZHH4b_LO(VVHH_Vertex_LO):
-    _pattern = r'ZHHTo4B_' + VVHH_Vertex_LO._pattern
+    search_pattern = re.compile(r'ZHHTo4B_' + VVHH_Vertex_LO.search_pattern)
+    format_pattern = 'ZHHTo4B_CV_{CV}_C2V_{C2V}_C3_{C3}'
 
 class WHH4b_LO(VVHH_Vertex_LO):
-    _pattern = r'WHHTo4B_' + VVHH_Vertex_LO._pattern
+    search_pattern = re.compile(r'WHHTo4B_' + VVHH_Vertex_LO.search_pattern)
+    format_pattern = 'WHHTo4B_CV_{CV}_C2V_{C2V}_C3_{C3}'
 
 class VHH4b_LO(VVHH_Vertex_LO):
-    _pattern = r'VHHTo4B_' + VVHH_Vertex_LO._pattern
+    search_pattern = re.compile(r'VHHTo4B_' + VVHH_Vertex_LO.search_pattern)
